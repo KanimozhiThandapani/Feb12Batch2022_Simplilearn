ab991f6de5194a3260a4363e59b80b5aeba27c09
Kumaresan Dhandapani <kumaresan.dhandapani@fairfaxmedia.com.au>
2022-02-19 12:51:10 +0530

    Program1Comitted

---
+++ Program1/src/com/homework/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.homework;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		int i;
+		for(i=1;i<=5;i++) {
+			System.out.println("Hello!!");
+		}
+
+	}
+
+}
