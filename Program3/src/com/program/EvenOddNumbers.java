43dcca3aa3ab6dce791be285418e53a1dd6c1921
Kanimozhi Thandapani <kanimozhit34@gmail.com>
2022-02-19 13:06:46 +0530

    Program3 CheckIn

---
+++ Program3/src/com/program/EvenOddNumbers.java
@@ -0,0 +1,15 @@
+package com.program;
+
+public class EvenOddNumbers {
+
+	public static void main(String[] args) {
+		int x=101;
+		if(x%2==0) {
+			System.out.println("Even Number");
+		}else {
+			System.out.println("Odd Number");
+		}
+
+	}
+
+}
